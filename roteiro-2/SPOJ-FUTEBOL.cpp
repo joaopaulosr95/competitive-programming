9a40ad2c0a8f3356e83bcb811c3f094b0039ad10
João Paulo S R Bastos <joaopaulosr95@users.noreply.github.com>
2016-03-20 21:14:48 -0300

    Defined storage struct for SPOJ-FUTEBOL

--- roteiro-2/SPOJ-FUTEBOL.cpp
+++ roteiro-2/SPOJ-FUTEBOL.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
 
+using namespace std;
+
+struct team {
+    char name[15];
+    int points;
+    int games;
+    int goals_pro;
+    int goals_con;
+    int goals_bal;
+    double percentage;
+};
+
+int main ()
+{
+    map<int,struct team> soccer;
+    int T, G, i;
+    char x;
+
+    while (cin >> T >> G && (T != 0 && G != 0))
+    {
+        
+    }
+    return 0;
+}
