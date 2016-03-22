f1f7f3bee4ecf9266b0ce9f6f605daf92ab9511c
João Paulo S Ribeiro Bastos <joaopaulosr95@gmail.com>
2016-03-22 16:28:58 -0300

    Final SPOJ-Jaspion

---
+++ roteiro-2/SPOJ-JASPION.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int main ()
+{
+    map<string, string> translate;
+    string jap, ptbr, input, output;
+    size_t space;
+    int T, M, N, i;
+
+    cin >> T;
+    while (T > 0)
+    {
+        cin >> M >> N;
+        cin.ignore(1);
+        for (i = 0; i < M; i++)
+        {
+            getline(cin, jap);
+            getline(cin, ptbr);
+            translate.insert(pair<string, string>(jap,ptbr));
+            //cout << jap << " : " << ptbr << endl;
+            jap.clear();
+            ptbr.clear();
+        }
+        for (i = 0; i < N; i++)
+        {
+            getline(cin,input);
+            istringstream iss(input);
+            while (iss >> jap)
+            {
+                map<string, string>::iterator found = translate.find(jap);
+                if (found != translate.end())
+                    output = output + translate[jap] + " ";
+                else
+                    output = output + jap + " ";
+            }
+            cout << output << endl;
+            output.clear();
+            input.clear();
+            jap.clear();
+        }
+        T -= 1;
+        translate.clear();
+    }
+    return 0;
+}
