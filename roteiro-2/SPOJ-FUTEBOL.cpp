4e1983c7476bbdfcc32ce04a0dca1633e46daa85
João Paulo S Ribeiro Bastos <joaopaulosr95@gmail.com>
2016-03-22 00:51:47 -0300

    Updated SPOJ-FUTEBOL

--- roteiro-2/SPOJ-FUTEBOL.cpp
+++ roteiro-2/SPOJ-FUTEBOL.cpp
@@ -3,7 +3,6 @@
 #include <algorithm>
 #include <string>
 #include <cstring>
-#include <cctype>
 #include <vector>
 #include <map>
 
@@ -15,7 +14,7 @@ string strToLow (const string &a)
     int i;
     string low = a;
 
-    for (i = 0; i < a.size(); i++)
+    for (i = 0; i < a.size(); ++i)
         low[i] = tolower(a[i]);
 
     return low;
@@ -35,7 +34,7 @@ struct team
     {
         name = N;
         points = 0;
-        points_perc = 0;
+        points_perc = 0.0;
         games = 0;
         goals_pro = 0;
         goals_con = 0;
@@ -57,9 +56,7 @@ struct team
     void setPP()
     {
         if (games > 0)
-            points_perc = 1.0 * ((100 * points) / (3 * games));
-        else
-            points_perc = -1.0;
+            points_perc = (100.0 * points) / (3.0 * games);
     }
 
     void printInfo(int pos)
@@ -67,25 +64,24 @@ struct team
         if (pos == -1)
             cout << "    ";
         else
-            cout << setw(2) << pos << "."; 
-
-        cout << setw(16) << name;
-        cout << setw(4) << points << games << goals_pro << goals_con << goals_bal;
+            cout << setw(2) << pos << ". "; 
+        cout << setw(15) << name << setw(4) << points << setw(4) << games;
+        cout << setw(4) << goals_pro << setw(4) << goals_con << setw(4) << goals_bal;
         if (games > 0)
             cout << setw(7) << fixed << setprecision(2) << points_perc;
         else
             cout << "    N/A";
-
         cout << endl;
     }
 
     //Define the 'equal-equal' operator to handle the struct variables
     bool operator == (const struct team &T)
     {
-        return T.points == this->points && 
-            T.goals_pro == this->goals_pro &&
-            T.goals_bal == this->goals_bal && 
-            strToLow(T.name) == strToLow(this->name);
+        if (T.points == points && T.goals_pro == goals_pro &&
+            T.goals_bal == goals_bal)
+            return true;
+        else
+            return false;
     }
 };
 
@@ -93,47 +89,64 @@ struct team
 bool compare (const struct team& T1, const team& T2)
 {
     if (T1.points != T2.points)
-        return T1.points < T2.points;
+    {
+        if (T1.points > T2.points)
+            return true;
+        else
+            return false;
+    }
     else if (T1.goals_bal != T2.goals_bal)
-        return T1.goals_bal < T2.goals_bal;    
+    {
+        if (T1.goals_bal > T2.goals_bal)
+            return true;
+        else
+            return false;
+    }
     else if (T1.goals_pro != T2.goals_pro)
-        return T1.goals_pro < T2.goals_pro;
+    {
+        if (T1.goals_pro > T2.goals_pro)
+            return true;
+        else
+            return false;
+    }
     else
     {
-        string a = strToLow(T1.name),
-                b = strToLow(T2.name);
-        return a > b;
+        if (strToLow(T1.name) <= strToLow(T2.name))
+            return true;
+        else
+            return false;
     }
 }
 
 // Main program
 int main ()
 {
-    map<string,int> teams;
-    vector<struct team> teamsInfo(28);
+    map<string, int> teams;
+    vector<struct team> teamsInfo;
     int T, G, i, T1g, T2g;
     string T1name, T2name;
     char c;
     
-    while (cin >> T >> G && (T != 0 && G != 0))
+    while (cin >> T >> G && T != 0 && G != 0)
     {
-        for (i = 0; i < T; i++)
+        for (i = 0; i < T; ++i)
         {
             cin >> T1name;
             struct team T1(T1name);
-            teams[T1name] = i;
+            teams.insert(make_pair(T1name, i));
             teamsInfo.push_back(T1);
         }
-        for (i = 0; i < G; i++)
+        for (i = 0; i < G; ++i)
         {
             cin >> T1name >> T1g >> c >> T2g >> T2name;
             teamsInfo[teams[T1name]].addGame(T1g, T2g);
             teamsInfo[teams[T2name]].addGame(T2g, T1g);    
         }
         sort(teamsInfo.begin(), teamsInfo.end(), compare);
-        for (i = 0; i < T; i++)
+        for (i = 0; i < T; ++i)
         {
-            if (i > 0 && teamsInfo[i] == teamsInfo[i+1])
+            teamsInfo[i].setPP();
+            if (i > 0 && teamsInfo[i] == teamsInfo[i-1])
                 teamsInfo[i].printInfo(-1);
             else
                 teamsInfo[i].printInfo(i+1);
