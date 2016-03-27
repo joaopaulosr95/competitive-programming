9debefa249c8cec2b8389261837df51d334bac68
João Paulo S Ribeiro Bastos <joaopaulosr95@gmail.com>
2016-03-27 10:50:30 -0300

    Final SPOJ-PEDAGIO.cpp

--- roteiro-5/SPOJ-PEDAGIO.cpp
+++ roteiro-5/SPOJ-PEDAGIO.cpp
@@ -7,25 +7,24 @@ using namespace std;
 
 int main ()
 {
-  int testNumber, C, E, L, P, adjMatrix[51][51], k, fromV, toV, dist[51];
+  int testNumber, C, E, L, P, k, fromV, toV, dist[51];
+  bool adjMatrix[51][51];
   vector<int> solution;
-  bool visited[51];
+  queue<int> toVisit;
 
   testNumber = 1;
   while ((cin >> C >> E >> L >> P) &&
       (C != 0 && E != 0 && L != 0 && P != 0))
   {
-    for (fromV = 1; fromV <= E; fromV++)
-      for (toV = 1; toV <= E; toV++)
-        adjMatrix[fromV][toV] = 0;
-    
+    for (fromV = 1; fromV <= C; fromV++)
+      for (toV = 1; toV <= C; toV++)
+        adjMatrix[fromV][toV] = false;
+
     for (k = 1; k <= E; k++)
     {
       cin >> fromV >> toV;
-      adjMatrix[fromV][toV] = 1;
-      adjMatrix[toV][fromV] = 1;
+      adjMatrix[fromV][toV] = adjMatrix[toV][fromV] = true;
     }
-    
 /*    for (fromV = 1; fromV <= C; fromV++)
     {
       for (toV = 1; toV <= C; toV++)
@@ -36,43 +35,38 @@ int main ()
     }
     cout << endl;
 */
-    
-    //----------------------------------------//
+    //------------------ DIJKSTRA -----------------//
     for (k = 1; k <= C; k++)
-    {
-      visited[k] = false;
-      dist[k] = 99999999;
-    }
-    
+      dist[k] = 99999999;  
     dist[L] = 0;
-    priority_queue< pair<int,int> > toVisit;
-    toVisit.push(make_pair(0,L));
+    toVisit.push(L);
     while (!toVisit.empty())
     {
-      toV = toVisit.top().second;
+      toV = toVisit.front();
       toVisit.pop();
-
       for (k = 1; k <= C; k++)
       {
-        if (adjMatrix[toV][k] == 1 && dist[toV] + 1 <= dist[k])
+        if (adjMatrix[toV][k] && k != L && dist[k] == 99999999)
         {
           dist[k] = dist[toV] + 1;
-          toVisit.push(make_pair(dist[k],k));
+          toVisit.push(k);
         }
       }
     }
-
     for (k = 1; k <= C; k++)
       if (dist[k] <= P && k != L)
         solution.push_back(k);
-    //----------------------------------------//
-
-    sort(solution.begin(),solution.end());
+    //---------------------------------------------//
+    //sort(solution.begin(),solution.end());
     cout << "Teste " << testNumber++ << endl;
     for (k = 0; k < solution.size(); k++)
-      cout << solution[k] << " ";
-    cout << endl;
-
+    {
+      if (k == 0)
+        cout << solution[k];
+      else
+        cout << " " << solution[k];
+    }
+    cout << endl << endl;
     vector<int>().swap(solution);
   }
   return 0;
