f0697050b1a027972d1a59afdafeede156fb7cae
apretti93 <apretti93@gmail.com>
2016-04-10 18:36:10 -0300

    Update SPOJ-DUENDE.cpp

--- roteiro-5/SPOJ-DUENDE.cpp
+++ roteiro-5/SPOJ-DUENDE.cpp
@@ -14,19 +14,20 @@ int matrizCaverna[11][11];
 
 int search(int l, int c)
 {
-    
+
     int iniciomat = 0;
     int finalmat = 0;
-    int larguras[100], comprimentos[100], origem[100];
     int distancia = 0;
+    int larguras[1000], comprimentos[1000], origem[10000];
 
     larguras[finalmat]=l;
     comprimentos[finalmat]=c;
     origem[finalmat]=0;
+    
     finalmat++;
 
     while (iniciomat!=finalmat){
-            
+
         l=larguras[iniciomat];
         c=comprimentos[iniciomat];
         distancia=origem[iniciomat];
@@ -39,47 +40,48 @@ int search(int l, int c)
                 int aux1=l+mx[i];
                 int aux2=c+my[i];
                 if (matrizCaverna[aux1][aux2]<2 && aux1 >= 0 && aux1 < N && aux2 >= 0 && aux2< M){
-                    
-                    
+
+
                     larguras[finalmat]=aux1;
                     comprimentos[finalmat]=aux2;
                     origem[finalmat]=distancia++;
                     finalmat++;
                 }
 
-            }
+                    }
             }
 
         matrizCaverna[l][c] = 2;
                 }
-    
+
         return -1;
-        
-                }
+
+                    }
 
 int main()
 {
-    int i, j,aux3,aux4;
-    
+
+    int i, j,x,y;
+
     cin >> N >> M;
-    
+
     for (i=0;i < N;i++){
-            
-        for (j=0;j<M;j++;){
-            
+
+        for (j=0;j<M;j++){
+
             cin >> matrizCaverna[i][j];
             if (matrizCaverna[i][j] == 3){
-                aux3=i;
-                aax4=j;
+                x=i;
+                y=j;
                 matrizCaverna[i][j]=1;
-    
+
             }
         }
         }
 
             cout << search(x,y) << endl;
-            
-            
+
+
         return 0;
 }
 
