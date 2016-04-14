f8170a14fd7df5bbd5511ffab851cfc825e8879a
João Paulo S Ribeiro Bastos <joaopaulosr95@gmail.com>
2016-04-14 16:21:24 -0300

    Final SPOJ-Knapsack.cpp

--- roteiro-4/SPOJ-KNAPSACK.cpp
+++ roteiro-4/SPOJ-KNAPSACK.cpp
@@ -1,11 +1,41 @@
 #include <iostream>
 #include <vector>
 
-void buildKnapsack ()
-{
+using namespace std;
 
+vector<pair<int,int> > items; //<Weight,Value>
+
+int solve (int size, int nitems)
+{
+    int knapsack[nitems+1][size+1], i, k;
+    
+    for (i = 0; i <= nitems; i++)
+    {
+        for (k = 0; k <= size; k++)
+        {
+            if (i == 0 || k == 0)
+               knapsack[i][k] = 0;
+            else if (items[i-1].first <= k)
+                knapsack[i][k] = max(items[i-1].second + knapsack[i-1][k - items[i-1].first], 
+                                knapsack[i-1][k]);
+            else
+                knapsack[i][k] = knapsack[i-1][k];
+        }
+    }
+    
+    return knapsack[nitems][size];
 }
 
 int main ()
 {
+    int size, nitems, i, W, V;
+
+    cin >> size >> nitems;
+    for (i = 0; i < nitems; i++)
+    {
+        cin >> W >> V;
+        items.push_back(make_pair(W,V));
+    }
+    
+    cout << solve(size, nitems) << endl;
 }
