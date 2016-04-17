e0f86c494580e96c38efb11ee59595d97e102fc8
João Paulo Bastos <joaopaulosr95@users.noreply.github.com>
2016-04-17 20:59:01 -0300

    Final SPOJ-MINIMO.cpp
    Em acordo com o apretti93, o programa foi mudado da linguagem C para C++

--- roteiro-6/SPOJ-MINIMO.cpp
+++ roteiro-6/SPOJ-MINIMO.cpp
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <iostream>
 #include <algorithm>
 
 using namespace std;
@@ -7,46 +7,46 @@ using namespace std;
 #define LIM1 1000000000
 
 int main() {
-    int n, m;
-	int g[LIM][LIM][LIM];
-    int num = 0;
-    
-    while (scanf("%d %d", &n, &m) != EOF) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                g[i][j][0] = LIM1;
-            }
-        }
-        
-        for (int i = 0; i < n; i++) g[i][i][0] = 0;
-        
-        for (int i = 0; i < m; i++) {
-            int u, v, c;
-            scanf("%d %d %d", &u, &v, &c);
-            
-            g[u-1][v-1][0] = min(g[u-1][v-1][0], c);
-        }
-        
-        for (int k = 0; k < n; k++) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    g[i][j][k+1] = min(g[i][k][k] + g[k][j][k], g[i][j][k]);
-                }
-            }
-        }
-        
-        int c;
-        scanf("%d", &c);
-        
-        printf("Instancia %d\n", ++num);
-        for (int i = 0; i < c; i++) {
-           int o, d, t;
-           scanf("%d %d %d", &o, &d, &t);
-           
-           printf("%d\n", g[o-1][d-1][t] == LIM1 ? -1 : g[o-1][d-1][t]);
-        }
-        printf("\n");
-    }
-    
-    return 0;
+	int n, m, g[LIM][LIM][LIM], num = 0;
+
+	while (cin >> n >> m) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				g[i][j][0] = LIM1;
+			}
+		}
+
+		for (int i = 0; i < n; i++) 
+			g[i][i][0] = 0;
+
+		for (int i = 0; i < m; i++) {
+			int u, v, c;
+			cin >> u >> v >> c;
+			g[u-1][v-1][0] = min(g[u-1][v-1][0], c);
+		}
+
+		for (int k = 0; k < n; k++) {
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					g[i][j][k+1] = min(g[i][k][k] + g[k][j][k], g[i][j][k]);
+				}
+			}
+		}
+
+		int c;
+		cin >> c;
+
+		cout << "Instancia " << ++num << "\n";
+		for (int i = 0; i < c; i++) {
+			int o, d, t;
+			cin >> o >> d >> t;
+			if (g[o-1][d-1][t] == LIM1)
+				cout << "-1\n";
+			else
+				cout << g[o-1][d-1][t] << "\n";
+		}
+		cout << "\n";
+	}
+
+	return 0;
 }
