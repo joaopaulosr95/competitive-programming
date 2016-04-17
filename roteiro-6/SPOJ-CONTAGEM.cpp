092bab54a5e236572d041e54478bb3013ba1c91f
apretti93 <apretti93@gmail.com>
2016-04-17 19:05:02 -0300

    Create SPOJ-CONTAGEM.cpp

---
+++ roteiro-6/SPOJ-CONTAGEM.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+#include <iostream>     // std::cout
+#include <algorithm>    // std::sort
+#include <vector>       // std::vector
+#include <string>
+#include <iterator>
+#include <cstring>
+
+char palavra[61];
+
+int main() {
+
+    unsigned long long int result;
+
+	int num = 0;
+
+    while (scanf("%s", palavra) != EOF) {
+
+		result=0;
+
+        int siz3=strlen(palavra);
+
+        for (int i=0; i<siz3; i++) {
+
+            result=result*2+palavra[i]-'a';
+
+	   }
+
+        printf("Palavra %d\n", ++num);
+        printf("%llu\n\n", result);
+    }
+
+    return 0;
+}
