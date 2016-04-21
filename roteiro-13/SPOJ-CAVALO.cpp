65fc99c951a345ba2a1c9a8c506e3e7fad706644
apretti93 <apretti93@gmail.com>
2016-04-21 16:44:57 -0300

    Update SPOJ-CAVALO.cpp

--- roteiro-13/SPOJ-CAVALO.cpp
+++ roteiro-13/SPOJ-CAVALO.cpp
@@ -21,10 +21,10 @@ bool buscaP(int s, int t, int size){
     for(v = 0; v < size; v++){
         if(g[s][v] - f[s][v] > 0 && !vist[v])
         {
-            pred[v] = s;
-            if(buscaP(v, t, size)){
-			return true;
-			}
+        pred[v] = s;
+        if(buscaP(v, t, size)){
+	return true;
+	}
         }
     }
     return false;
@@ -47,54 +47,52 @@ int ff_flow(int size, int s, int t){
             deltaPath = min(deltaPath, g[pred[c]][c] - f[pred[c]][c]);
       }
       for(c = t; pred[c] != c; c = pred[c]){
-            f[pred[c]][c] += deltaPath;
-            f[c][pred[c]] -= deltaPath;
+            f[pred[c]][c] = f[pred[c]][c] + deltaPath;
+            f[c][pred[c]] = f[c][pred[c]] - deltaPath;
       }
         memset(vist, false, sizeof(bool)*size);
         aux = buscaP(s, t, size);
     }
     int sum = 0;
-    for(i = 0; i < size; i++)
-    {
-        sum += f[i][t];
+    for(i = 0; i < size; i++){
+    sum = sum + f[i][t];
     }
     return sum;
-}
+    }
 
-int main()
-{
+int main(){
     int cav,sold,afin, num;
     for(num = 1; true; num++)
-    {
-		cin >> cav >> sold >> afin;
+        {
+	cin >> cav >> sold >> afin;
         if(std::cin.eof()){ 
-        	break;
+        break;
         }
-		cout << "Instancia " << num << endl;
+	cout << "Instancia " << num << endl;
         int c, i;
 
 
         for(i = 0; i < cav + sold + 2; i++)
         {
-            memset(g[i], 0, sizeof(int)*(cav+sold+2));
+        memset(g[i], 0, sizeof(int)*(cav+sold+2));
         }
 
         for(i = 1; i <= cav; i++){
-			cin >> c;
-            g[sold + i][cav + sold + 1] = c;
+	cin >> c;
+         g[sold + i][cav + sold + 1] = c;
         }
 
         int u, v;
 
         for(i = 0; i < afin; i++)
         {
-			cin >> u >> v;
-            g[v][ sold + u] = 1;
+	cin >> u >> v;
+        g[v][ sold + u] = 1;
         }
 
         for(i = 1; i <= sold; i++)
         {
-            g[0][i] = 1;
+        g[0][i] = 1;
         }
 
 	cout << ff_flow(sold + cav + 2, 0, sold + cav + 1) << endl << endl;
