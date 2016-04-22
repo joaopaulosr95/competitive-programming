8a6b61e6915f1b05fabdc7d55ef6d1c9c238a280
João Paulo Bastos <joaopaulosr95@users.noreply.github.com>
2016-04-22 17:12:28 -0300

    Final UVa-11385.cpp

--- roteiro-8/UVa-11385.cpp
+++ roteiro-8/UVa-11385.cpp
@@ -7,19 +7,27 @@
 
 using namespace std;
 
-const int MAX = 100;
-vector<int> fibonacci {1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 
-    233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 
-    46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269, 
-    2178309, 3524578, 5702887, 9227465, 14930352, 24157817, 39088169, 
-    63245986, 102334155, 165580141, 267914296, 433494437, 701408733, 
+const int fibonacci[45] = {1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,
+    233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657,
+    46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269,
+    2178309, 3524578, 5702887, 9227465, 14930352, 24157817, 39088169,
+    63245986, 102334155, 165580141, 267914296, 433494437, 701408733,
     1134903170, 1836311903};
+
+map<int,char> createMap()
+{
+    map<int,char> m;
+    for (int i = 0; i < 45; i++)
+        m[fibonacci[i]] = ' ';
+    return m;
+}
 
 int main ()
 {
     int T, N, code, pos, max;
+    map<int,char> fibmap;
+    vector<int> fibcodes;
     string cipher;
-    map<int,int> fibmap;
 
     cin >> T;
     while(T--)
@@ -29,11 +37,7 @@ int main ()
         for (int i = 0; i < N; i++)
         {
             cin >> code;
-            vector<int>::iterator it = find(fibonacci.begin(), 
-                    fibonacci.end(), code);
-            pos = distance(fibonacci.begin(), it);
-            fibmap.insert(make_pair(code, pos));
-            //cout << "Code: " << code << " Pos: " << pos << endl;
+            fibcodes.push_back(code);
             if (max < code)
                 max = code;
         }
@@ -42,18 +46,26 @@ int main ()
         getline(cin, cipher);
         //cout << cipher << "\n";
 
-        for (vector<int>::iterator it = fibonacci.begin(); 
-                it != fibonacci.end() && *it <= max; it++)
+        fibmap = createMap();
+        for (int i = 0, j = 0, k = 0; i < cipher.length() &&
+            j < fibcodes.size(); i++)
         {
-            map<int,int>::iterator mit = fibmap.find(*it);
-            if (mit != fibmap.end() && isupper(cipher[mit->second]))
-                cout << cipher[mit->second];
+            if (isupper(cipher[i]))
+            {
+                fibmap[fibcodes[i-k]] = cipher[i];
+                j++;
+            }
             else
-                cout << ' ';
+                k++;
         }
+
+        for (int i = 0; fibonacci[i] <= max; i++)
+            cout << fibmap[fibonacci[i]];
         cout << "\n";
-        fibmap.clear();
+
         cipher.clear();
+        map<int,char>().swap(fibmap);
+        vector<int>().swap(fibcodes);
     }
     return 0;
 }
