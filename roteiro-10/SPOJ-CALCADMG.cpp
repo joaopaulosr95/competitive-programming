19d0bfd7c661cfe8575d5d6dd2cf71a5094b6587
João Paulo S Ribeiro Bastos <joaopaulosr95@gmail.com>
2016-05-02 14:09:52 -0300

    Final SPOJ-CALCADMG.cpp

--- roteiro-10/SPOJ-CALCADMG.cpp
+++ roteiro-10/SPOJ-CALCADMG.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int eucl (int a, int b)
+{
+    return b == 0 ? a : eucl(b, a % b);
+}
+
+int main ()
+{
+    int N, x1, y1, x2, y2;
+
+    cin >> N;
+    while (N--)
+    {
+        cin >> x1 >> y1 >> x2 >> y2;
+
+        cout << 1 + eucl(abs(x2 - x1), abs(y2 - y1)) << "\n";
+    }
+    return 0;
+}
 
--- roteiro-8/SPOJ-ENCOTEL.cpp
+++ roteiro-8/SPOJ-ENCOTEL.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <cstdio>
 
+using namespace std;
+
+
+int main ()
+{
+    char ch;
+    
+    while (scanf("%c", &ch) != EOF)
+    {
+        if (ch >= 65 && ch <= 67)
+            cout << "2";
+        else if (ch >= 68 && ch <= 70)
+            cout << "3";
+        else if (ch >= 71 && ch <= 73)
+            cout << "4";
+        else if (ch >= 74 && ch <= 76)
+            cout << "5";
+        else if (ch >= 77 && ch <= 79)
+            cout << "6";
+        else if (ch >= 80 && ch <= 83)
+            cout << "7";
+        else if (ch >= 84 && ch <= 86)
+            cout << "8";
+        else if (ch >= 87 && ch <= 90)
+            cout << "9";
+        else
+            cout << ch;
+    }
+
+    return 0;
+}
