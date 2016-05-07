3a09da0684362ec2443433aeddc61b54e2a40a82
apretti93 <apretti93@gmail.com>
2016-05-07 19:45:45 -0300

    Update SPOJ-CAIXAS.cpp

--- roteiro-12/SPOJ-CAIXAS.cpp
+++ roteiro-12/SPOJ-CAIXAS.cpp
@@ -9,24 +9,23 @@ using namespace std;
 int main(){
 
     int N,P;
-    int **matriz;
+    int matriz[1000][1000];
     int guardaLinha,guardaColuna,tam;
     while(cin >> N >> P && N!=0 && P!=0){
     int cnt=0;
     int cntauxEsquerda=0,cntauxDireita=0;
-    matriz= new int *[P];
+  // matriz= new int *[P];
 
-    for(int i=0;i<P;i++){
+    for(int i=0;i<1000;i++){
 
-        matriz[i]=new int[N];
-
-        for(int j=0;j<N;j++){
+        for(int j=0;j<1000;j++){
 
             matriz[i][j]= 0;
 
         }
 
     }
+
 
 
     for(int k=0;k<P;k++){
@@ -38,11 +37,12 @@ int main(){
 
     }
 
+   
+
    /* for (int l1=0;l1<P;l1++){
     	cout << endl;
     	for (int c1=0;c1<N;c1++){
     		cout << matriz[l1][c1] << " " ;
-
     	}
     }*/
 
@@ -54,6 +54,8 @@ int main(){
                 if(matriz[r][aux]==1){
                     guardaLinha=r;
                     guardaColuna=aux;
+                    aux++;
+                    //cout << "Linha é " << guardaLinha << "e Coluna é " << guardaColuna << endl;
                         if(matriz[r][aux]!=0){
                             cnt++;
                             aux++;
@@ -64,17 +66,24 @@ int main(){
     }
 
 
+   // cout << "CNT até aqui é " << cnt << endl;
+
+
     int colunaNova=guardaColuna;
+    if(guardaLinha!=0){
     while(matriz[guardaLinha-1][colunaNova]!=0){
         colunaNova++;
         cntauxDireita++;
-    }
+    }}
+    else cntauxDireita=0;
 
     int colunaNova1=guardaColuna;
+    if(guardaLinha!=P){
     while(matriz[guardaLinha+1][colunaNova1]!=0){
         colunaNova1++;
         cntauxEsquerda++;
-    }
+    }}
+    else cntauxEsquerda=0;
 
 
     if(cntauxEsquerda<=cntauxDireita) cnt=cnt+cntauxEsquerda;
@@ -82,12 +91,20 @@ int main(){
 
     cout << cnt << endl;
 
-    for (int w=0; w < N; w++) {
-        delete matriz[w];
-    }
-    delete [] matriz ;
-    }
+  /*  for (int w=0; w < P; w++) {
+        for (int y=0; y < N; y++) {
+        delete matriz[w][y];
+    }*/
+      
 
+    //for(int u=0;u<P;u++){
+    //delete [] matriz ;
+  //  } 
+
+    
+
+
+    }
 
     return 0;
 
