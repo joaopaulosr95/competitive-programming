5668d5df1aaa3b6d7c35c0fe3cf5c1b95120ae26
apretti93 <apretti93@gmail.com>
2016-05-16 16:48:30 -0300

    Update SPOJ-BICHO.cpp

--- roteiro-10/SPOJ-BICHO.cpp
+++ roteiro-10/SPOJ-BICHO.cpp
@@ -1,6 +1,6 @@
-#include <iostream>     // std::cout
-#include <algorithm>    // std::sort
-#include <vector>       // std::vector
+#include <iostream> // std::cout
+#include <algorithm> // std::sort
+#include <vector> // std::vector
 #include <string>
 #include <iterator>
 #include <cstdio>
@@ -9,32 +9,28 @@
 
 using namespace std;
 
+int N,M;
+double V;
+
 int main(){
-	int N,M;
-	double V,aux;
-	while((cin >> V >> N >> M) && (!V==0 && !N==0 && !M==0)){
-    if(N%10000==M%10000) {aux=3000*V;cout << "if 1" << endl;}
-    else if(N%1000==M%1000){ aux=500*V;cout <<"if 2" << endl;}
-    else if(N%100==M%100) {aux=50*V; cout << "if 3" << endl;}
-    else {
-     N=N%100;
-     M=M%100;
 
-     if(N%4==M%4){
-            cout << "if 4" << endl;
-        if(N%4!=0 && (N/4==M/4)) aux=16*V;
-        else if(N%4==0 && ((N/4)+1==(M/4) || (N/4)==(M/4)+1)) aux=16*V;
-        else aux=0;
-     }
+  while(cin >> V >> N >> M){
+      if(V==0 && N==0 && M==0) {
+        break;
+      }
 
-    }
+      if(N%10000==M%10000) std::cout << std::fixed << std::setprecision(2) << V*3000 << std::endl;
+      else if(N%1000==M%1000) std::cout << std::fixed << std::setprecision(2) << V*500 << std::endl;
+      else if(N%100==M%100) std::cout << std::fixed << std::setprecision(2) << V*50 << std::endl;
 
+      else {
+        N=N%100;
+        M=M%100;
+          if(N%4!=0 && M%4!=0 && (N/4==M/4)) std::cout << std::fixed << std::setprecision(2) << V*16 << std::endl;
+          else if ((N%4==0 && N/4==(M/4)+1) || (M%4==0 && M/4==(N/4)+1)) std::cout << std::fixed << std::setprecision(2) << V*16 << std::endl;
+          else std::cout << std::fixed << std::setprecision(2) << V*0 << std::endl;
 
-
-
-	std::cout << std::fixed << std::setprecision(2) << aux << std::endl;
-
-	}
-
-	return 0;
+      }
+  }
+  return 0;
 }
