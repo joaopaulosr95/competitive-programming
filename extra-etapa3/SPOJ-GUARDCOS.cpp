3b212766cedc31179e3607bb0183da0d2b8da613
Joao Paulo Bastos <joaopaulosr95@gmail.com>
2016-06-10 13:31:49 -0300

    Final SPOJ-GUARDCOS.cpp

--- extra-etapa3/SPOJ-GUARDCOS.cpp
+++ extra-etapa3/SPOJ-GUARDCOS.cpp
@@ -3,11 +3,11 @@
 
 using namespace std;
 
-int main () {
+int main() {
     double a, b, c;
 
-    while (cin >> a >> c >> b) {
-        if ((12.0 / c) >= (sqrt(144 + pow(a,2))/b))
+    while(cin >> a >> c >> b) {
+        if((12.0 / c) >= (sqrt(144 + pow(a,2))/b))
             cout << "S\n";
         else
             cout << "N\n";
