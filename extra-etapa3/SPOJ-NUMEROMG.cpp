9de84bc20b83ac47bd3c9bbe54f28612b46995ea
João Paulo Bastos <joaopaulosr95@users.noreply.github.com>
2016-06-11 16:08:34 -0300

    Final SPOJ-NUMEROMG.cpp

---
+++ extra-etapa3/SPOJ-NUMEROMG.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <cmath>
+
+using namespace std;
+const string __ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+unsigned int base36decode (string input)
+{
+	int i;
+	unsigned int base10 = 0;
+	size_t baseIndex;
+
+	for (i = 0; i < input.length(); i++)
+	{
+		input[i] = toupper(input[i]);
+		baseIndex = __ALPHABET.find(input[i]);
+		base10 += baseIndex * pow(36, input.length()-i-1);
+	}
+	//cout << base10 << endl;
+	return base10;
+}
+
+string base36encode (unsigned int base10)
+{
+	string base36;
+
+	do
+	{
+		base36 = __ALPHABET[base10 % 36] + base36;
+	} while(base10 /= 36);
+	//cout << base36 << endl;
+	return base36;
+}
+
+int main ()
+{
+	string a, b;
+
+	while (cin >> a >> b && ( a != "0" && b != "0" ))
+		cout << base36encode(base36decode(a) + base36decode(b)) << endl;
+	return 0;
+}
